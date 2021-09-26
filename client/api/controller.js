7b54114240750ee490b7aafb8f134a10abd419d0
DimitriDeren <dimitri.deren@gmail.com>
2021-09-26 18:27:24 +1300

    peer programming Gio Cigaral & Dimitri Ariadi
    added controller.js & config for API post request

---
+++ client/api/controller.js
@@ -0,0 +1,15 @@
+import config from "../config/config.js"
+
+export async function getRequest(queryParam){
+  let url = config.ACCESS_POINT;
+  url = url.concat(`&title=${queryParam}`);
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+}
+
+export async function postRequest(recipeModel){
+    
+}
---
+++ client/config/config.js
@@ -0,0 +1,9 @@
+
+export default CONFIG = {
+  ACCESS_POINT : "https://localhost:5000/recipes/v2?",
+  APP_KEY : "65c1c52b71c53e458dee4848043259ce",
+  APP_ID : "c7aab75a"
+}
+
+
+  
