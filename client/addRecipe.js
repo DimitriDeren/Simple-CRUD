b5cc56aafd6c7b333901578d3655d592b62729c5
DimitriDeren <dimitri.deren@gmail.com>
2021-09-26 15:55:39 +1300

    added fonts and form functionality

--- client/addRecipe.js
+++ client/addRecipe.js
@@ -1,4 +1,13 @@
-function save() {
+function saveForm() {
   var arrInput = document.getElementsByTagName("input");
-  console.log(arrInput);
+
+  var title = document.querySelector('.title-input').value;
+  var ingredient = document.querySelector('.title-input').value;
+  var directions = document.querySelector('.title-input').value;
+  var image = document.querySelector('.image-input').value;
+
+  console.log("Title: " + title);
+  console.log("Ingredient: " + ingredient);
+  console.log("Directions: " + directions);
+  console.log("Image: " + image);
 }
