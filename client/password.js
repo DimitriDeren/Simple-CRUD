0c05c193a6c9217f5b9e84fa23d40d77428e9334
DimitriDeren <dimitri.deren@gmail.com>
2021-09-29 15:08:47 +1300

    fixed styling and toggle button

--- client/password.js
+++ client/password.js
@@ -3,11 +3,20 @@
     300466914 --> */
 
 // Toggles the password to show or hide the input from user *This doesn't work
-function togglePassword() {
-    var x = document.getElementById("loginPwd");
-    if (x.type === "password") {
+function togglePasswordRegister() {
+    var x = document.getElementById("registerPwd");
+    if (x.type === "password" && x.type !== null) {
       x.type = "text";
     } else {
       x.type = "password";
+    }
+  }
+
+  function togglePasswordLogin() {
+   var y = document.getElementById("loginPwd");
+    if (y.type === "password" && y.type !== null) {
+      y.type = "text";
+    } else {
+      y.type = "password";
     }
   }
