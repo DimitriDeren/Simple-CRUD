7ebe2b91e992015556bf152b049f033dfe7db937
DimitriDeren <dimitri.deren@gmail.com>
2021-10-10 17:17:20 +1300

    restructuring and added ejs

--- server/api/client.route.js
+++ server/api/client.route.js
@@ -1,14 +1,23 @@
 import express from 'express';
 
 import ClientController from './client.controller.js';
+import RecipesDAO from '../data_access_model/RecipesDAO'
+
 
 const router = express.Router();
 
-// TODO: add POST request to route
+
 router
     .route("/")
     .get((req, res) => {
-      res.render()
+    const { recipesList, totalNumRecipes } = await RecipesDAO.getRecipes({
+        filters,
+        page,
+        recipesPerPage,
+    });
+      res.render("dashboard", {
+        recipes: recipesList
+      })
     });
 
 router
