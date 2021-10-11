609c89b56498fb899259f0ee5202cab6b9f48554
johanahg <johanah.gloria@gmail.com>
2021-10-11 16:25:04 +1300

    initial password controller commit

---
+++ client/api/password.controller.js
@@ -0,0 +1,13 @@
+/* get login input after submission */
+function getLoginInput() {
+    console.log(document.getElementById('loginUser'));
+    console.log(document.getElementById('loginPwd'));
+
+    // const username = document.getElementById('loginUser');
+    // const password = document.getElementById('loginPwd');
+}
+
+/* hash password field */
+function hashPassword() {
+
+}
