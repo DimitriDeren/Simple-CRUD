04d8e75264ae0e5a66a1ac919ade110c971d776b
DimitriDeren <dimitri.deren@gmail.com>
2021-10-14 16:19:24 +1300

    deleted port in .env

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ MongoClient.connect(
         });
     });
 
-//Connect to Heroku
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
+// //Connect to Heroku
+// if (port == null || port == "") {
+//   port = 8000;
+// }
+// app.listen(port);
 
