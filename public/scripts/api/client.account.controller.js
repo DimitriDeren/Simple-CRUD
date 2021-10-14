88a0220aa04bb8cb7a193cc0eea32ec1141da69f
Jerome Skelton <jeromeskelton123@gmail.com>
2021-10-14 18:45:50 +1300

    login page basic cookie functionality

--- public/scripts/api/client.account.controller.js
+++ public/scripts/api/client.account.controller.js
@@ -18,7 +18,9 @@ function authenticationCall(userName, password) {
     console.log("username: " + userName);
     console.log("password: " + password);
     console.log("--------------------------");
-    getAuthenticationRequest(userName, password);
+    if (getAuthenticationRequest(userName, password)) {
+        setCookie("username", userName, 30);
+    }
 }
 
 function registrationCall(userName, password) {
@@ -49,10 +51,6 @@ async function getAuthenticationRequest(userName, password) {
 
     const authentication = await response.json();
 
-    if (user != "" && user != null) {
-        setCookie("username", user, 365);
-    }
-
     console.log(authentication);
 
 
@@ -76,4 +74,35 @@ async function postNewRegistration(userName, password) {
 
     console.log(response.status);
     //return response.status;
+}
+
+function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+        alert("Cookie found, Reroute to account page");
+    } else {
+        alert("No cookie, going to login page");
+    }
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
