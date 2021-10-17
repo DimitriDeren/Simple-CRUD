349549598e762ae226a54d1145c0a533fc7fd085
Gianpaolo Cigaral <gio.cigaral@gmail.com>
2021-10-17 16:45:59 +1300

    implement passport js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import passport from 'passport';
+import strategy from 'passport-local';
 
 import RecipeRoutes from './api/recipes.route.js';
 import AccountRoutes from './api/account.route.js';
 import ClientRoutes from './api/client.route.js';
+
+import Account from './models/account.js';
+
+let LocalStrategy = strategy.Strategy;
 
 const app = express();
 const __dirname = path.resolve();
@@ -14,6 +22,22 @@ app.set('views', './client');
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'nwen group project',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport config
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// Mongoose config
+mongoose.connect(process.env.APP_DB_URI);
 
 // Setup public route
 app.use(express.static(path.join(__dirname, 'public')));
