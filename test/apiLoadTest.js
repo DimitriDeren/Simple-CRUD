6c80e880f0e33505c440c1d2263d676dfa1fb02a
DimitriDeren <dimitri.deren@gmail.com>
2021-10-17 18:06:37 +1300

    added heroku test performance

--- test/apiLoadTest.js
+++ test/apiLoadTest.js
@@ -3,7 +3,7 @@ import loadtest from 'loadtest';
 describe('Recipe APIs', () => {
 
     describe('Performance Test - GET route /api/v1/recipes', () => {
-        it('It should return all recipes', function(done) {
+        it('GET Request from LocalHost', function(done) {
             this.timeout(1000 * 60);
 
             const options = {
@@ -25,29 +25,43 @@ describe('Recipe APIs', () => {
         });
     });
 
+    describe('Performance Test - GET route /api/v1/recipes', () => {
+      it('GET Request from Heroku', function(done) {
+          this.timeout(1000 * 60);
+
+          const options = {
+              "url": 'https://glacial-spire-74573.herokuapp.com/api/v1/recipes',
+              "maxSeconds": 10,
+              "concurrency": 25,
+              "method": 'GET',
+          };
+
+          loadtest.loadTest(options, function(error, result) {
+              if (error) {
+                  return console.error(`Got an error: ${error}`);
+              } else {
+                  console.log('Tests run successfully');
+                  console.log('Results: ', result);
+                  done();
+              }
+          });
+      });
+  });
+
     describe('Performance Test - POST route /api/v1/recipes', () => {
         it('It should create recipes', function(done) {
             this.timeout(1000 * 60);
 
             const options = {
                 "url": 'http://localhost:5000/api/v1/recipes',
-                "maxRequests": 5,
+                "maxRequests": 1,
                 "maxSeconds": 5,
                 "method": 'POST',
-                requestGenerator: (params, options, client, callback) => {
-                    let data = {
-                        title: "TestPost",
-                        ingredients: "Testing",
-                        directions: "Directions",
-                    }
-                    const message = '{"hi": "ho"}';
-                    options.headers['Content-Type'] = 'application/json';
-                    options.headers['Accept'] = 'application/json';
-                    options.body = data;
-                    const request = client(options, callback);
-                    request.write(message);
-                    return request;
-                }
+                "body":{
+                  title: "TestPost",
+                  ingredients: "Testing",
+                  directions: "Directions",
+              }
             };
 
             loadtest.loadTest(options, function(error, result) {
